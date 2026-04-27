29b2db71f74a1861afce838d049cb2f8b910cbb6
joeElieSarkis <joe.sarkis.792@gmail.com>
2026-04-27 12:43:11 +0300

    Remove deprecated wlasl pipeline and unused data

--- src/wlasl/check_wlasl_counts.py
+++
@@ -1,7 +0,0 @@
-import os
-
-base = os.path.join('data', 'WLASL', 'WLASL_raw')
-for cls in sorted(os.listdir(base)):
-    p = os.path.join(base, cls)
-    if os.path.isdir(p):
-        print(cls, len(os.listdir(p)))
